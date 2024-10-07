f514a3b852d49aca60092fc195d488ff5572852a
mr3nz1 <murenzi419@gmail.com>
2024-10-07 11:56:05 +0200

    chore: project setup

---
+++ authentication-service/src/main/java/com/team/authentication_service/AuthenticationServiceApplication.java
@@ -0,0 +1,12 @@
+package com.team.authentication_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class AuthenticationServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(AuthenticationServiceApplication.class, args);
+	}
+}
---
+++ sops-service/src/main/java/com/team/sop_management_service/SopManagementServiceApplication.java
@@ -0,0 +1,12 @@
+package com.team.sop_management_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SopManagementServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SopManagementServiceApplication.class, args);
+	}
+
+}
