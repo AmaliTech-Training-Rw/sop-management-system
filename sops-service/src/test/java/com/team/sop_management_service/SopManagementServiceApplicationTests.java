632afbaad9a8c8224421f24093c03f51e1b144b5
gertrude654 <gertrude.uzimana@amalitechtraining.org>
2024-10-07 15:11:11 +0200

    ft/sop service added

---
+++ sops-service/src/test/java/com/team/sop_management_service/SopManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team.sop_management_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SopManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
