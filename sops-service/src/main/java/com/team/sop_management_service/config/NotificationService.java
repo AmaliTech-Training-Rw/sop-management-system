0f1ad4d3963882cd31889d666541924c0a120992
kevin-mboni <kevin.mbonigaba@amalitechtraining.org>
2024-10-11 14:14:24 +0200

    fix: notification format

--- sops-service/src/main/java/com/team/sop_management_service/config/NotificationService.java
+++ sops-service/src/main/java/com/team/sop_management_service/config/NotificationService.java
@@ -44,6 +44,7 @@
 
 package com.team.sop_management_service.config;
 
+import com.team.sop_management_service.models.SOPCreation;
 import com.team.sop_management_service.models.SOPInitiation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
@@ -61,39 +62,36 @@ public class NotificationService {
     }
 
     public void notifyAuthor(SOPInitiation sop) {
-        String email = sop.getApprovalPipeline().getAuthor().getEmail();
-        String sopTitle = sop.getTitle();
-        String messageContent = String.format("You have been assigned as the Author for SOP titled: %s", sopTitle);
-        String notificationType = "EMAIL";
+        String recipient = sop.getApprovalPipeline().getAuthor().getEmail();
+        String message = String.format("You have been assigned as the Author for SOP titled: %s", sop.getTitle());
+        String type = "EMAIL";
         String subject = "SOP Author Assignment";
 
 
-        NotificationMessage message = new NotificationMessage(subject, email, sopTitle, messageContent, notificationType);
-        sendNotification(email, message);
+        NotificationMessage msg = new NotificationMessage(subject, recipient, message, type);
+        sendNotification(recipient, msg);
     }
 
     public void notifyReviewers(SOPInitiation sop) {
         sop.getApprovalPipeline().getReviewers().forEach(reviewer -> {
-            String email = reviewer.getEmail();
-            String sopTitle = sop.getTitle();
-            String notificationType = "EMAIL";
-            String messageContent = String.format("You have been assigned as the Reviewer for SOP titled: %s", sopTitle);
+            String recipient = reviewer.getEmail();
+            String type = "EMAIL";
+            String message = String.format("You have been assigned as the Reviewer for SOP titled: %s",sop.getTitle());
             String subject = "SOP Reviewer Assignment";
 
-            NotificationMessage message = new NotificationMessage(subject, email, sopTitle, messageContent, notificationType);
-            sendNotification(email, message);
+            NotificationMessage msg = new NotificationMessage(subject, recipient, message, type);
+            sendNotification(recipient, msg);
         });
     }
 
     public void notifyApprover(SOPInitiation sop) {
-        String email = sop.getApprovalPipeline().getApprover().getEmail();
-        String sopTitle = sop.getTitle();
-        String messageContent = String.format("You have been assigned as the Approver for SOP titled: %s", sopTitle);
-        String notificationType = "EMAIL";
+        String recipient = sop.getApprovalPipeline().getApprover().getEmail();
+        String message = String.format("You have been assigned as the Approver for SOP titled: %s",sop.getTitle());
+        String type = "EMAIL";
         String subject = "SOP Approver Assignment";
 
-        NotificationMessage message = new NotificationMessage(subject, email, sopTitle, messageContent, notificationType);
-        sendNotification(email, message);
+        NotificationMessage msg = new NotificationMessage(subject,recipient, message, type);
+        sendNotification(recipient, msg);
     }
 
     private void sendNotification(String email, NotificationMessage message) {
